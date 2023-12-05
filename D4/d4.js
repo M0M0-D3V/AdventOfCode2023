154a9033e837717bb5b0c65bcae8f020cf07c510
Monica Hong <momo.star@gmail.com>
2023-12-04 20:58:24 -0800

    d4 part 2 done

--- D4/d4.js
+++ D4/d4.js
@@ -36,8 +36,49 @@ const partOne = (rows) => {
 
 const partTwo = (rows) => {
   let total = 0;
+  let scratchMap = new Map(); // key: card# - value: count
+
+  for (let i = 0; i < rows.length; i++) {
+    let row = rows[i];
+    let cardNum = row.slice(4, row.indexOf(":")).match(/\d+/);
+
+    if (!scratchMap.has(parseInt(cardNum[0]))) {
+      scratchMap.set(parseInt(cardNum[0]), 1);
+    }
+
+    let winNums = row
+      .slice(row.indexOf(":") + 2, row.indexOf("|") - 1)
+      .match(/\d+\s*/g)
+      .map(Number);
+
+    let scratchNums = row
+      .slice(row.indexOf("|") + 2)
+      .match(/\d+\s*/g)
+      .map(Number);
+
+    let matching = 0;
+    for (let scratchNum of scratchNums) {
+      if (winNums.includes(scratchNum)) {
+        matching++;
+      }
+    }
+    // tabulate next card copies
+    for (let copy = 1; copy <= matching; copy++) {
+      let copyCard = parseInt(cardNum) + copy;
+
+      if (copyCard <= rows.length && !scratchMap.has(copyCard)) {
+        scratchMap.set(copyCard, 1);
+      }
+      let cardCount = scratchMap.get(parseInt(cardNum));
+      let copyCardCount = scratchMap.get(copyCard);
+      scratchMap.set(copyCard, copyCardCount + cardCount);
+    }
+  }
+  scratchMap.forEach((value) => {
+    total += value;
+  });
   return total;
 };
 
-console.log(`Part One: `, partOne(textRows));
+// console.log(`Part One: `, partOne(textRows));
 console.log(`Part Two: `, partTwo(textRows));
