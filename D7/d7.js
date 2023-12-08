f679b4baec48c5748945d63a1935bfd332367e13
Monica Hong <momo.star@gmail.com>
2023-12-08 03:16:16 -0800

    wip d7 p1

--- D7/d7.js
+++ D7/d7.js
@@ -1,22 +1,43 @@
 var fs = require("fs");
-var file = fs.readFileSync("./D7/d7.txt", "utf-8");
-// var file = fs.readFileSync("./D7/test.txt", "utf-8");
+// var file = fs.readFileSync("./D7/d7.txt", "utf-8");
+var file = fs.readFileSync("./D7/test.txt", "utf-8");
 var rows = file.split("\r\n");
 
-const partOne = (rows) => {
-  let rankBidMap = new Map();
+let rankBidMap = new Map();
+let cardTypes = {
+  fiveKind: new Map(),
+  fourKind: new Map(),
+  fullHouse: new Map(),
+  threeKind: new Map(),
+  twoPair: new Map(),
+  onePair: new Map(),
+  highCard: new Map(),
+};
+
+const partOne = () => {
   for (let i = 0; i < rows.length; i++) {
     let row = rows[i];
     line = row.split(" ");
     // get rank for each hand and add to map
     let hand = line[0];
-    // get hand rank
-    let rank = getRank(rows, hand);
-    // parse bid
     let bid = parseInt(line[1]);
-    // add rank and bid to map
-    rankBidMap.set(rank, bid);
+    // get hand type
+    let type = getType(hand);
+    // save to type map
+    saveType(type, hand, bid);
+    console.log(cardTypes);
   }
+  // get hand ranks
+  setRanks(cardTypes[fiveKind]);
+  setRanks(cardTypes[fourKind]);
+  setRanks(cardTypes[fullHouse]);
+  setRanks(cardTypes[threeKind]);
+  setRanks(cardTypes[twoPair]);
+  setRanks(cardTypes[onePair]);
+  setRanks(cardTypes[highCard]);
+  // let rank = getRank(hand);
+  // add rank and bid to map
+  // rankBidMap.set(rank, bid);
   let total;
   return total;
 };
@@ -37,12 +58,62 @@ const cardStrength = {
   2: 2,
 };
 
-const getRank = (rows, hand) => {
-  let count = 0;
-  for (let i = 0; i < rows.length; i++) {
-    // do the things here
+const getType = (hand) => {
+  let seen = {};
+  for (let char of hand) {
+    if (seen[char]) {
+      seen[char]++;
+    } else {
+      seen[char] = 1;
+    }
   }
-  return count + 1;
+
+  for (let count in seen) {
+    if (seen[count] === 5) {
+      return "fiveKind";
+    }
+    if (seen[count] === 4) {
+      return "fourKind";
+    }
+    if (seen[count] === 3) {
+      for (let c in seen) {
+        if (seen[c] === 2) {
+          return "fullHouse";
+        }
+      }
+      return "threeKind";
+    }
+    if (seen[count] === 2) {
+      for (let c in seen) {
+        if (seen[c] === 2 && c !== count) {
+          return "twoPair";
+        }
+        if (seen[c] === 3) {
+          return "fullHouse";
+        }
+      }
+      return "onePair";
+    }
+    if (seen[count] === 1) {
+      let ones = 0;
+      for (let c in seen) {
+        if (seen[c] === 1) {
+          ones++;
+        }
+      }
+      if (ones === 5) {
+        return "highCard";
+      }
+    }
+  }
 };
 
-console.log(`Part One: `, partOne(rows));
+const saveType = (type, hand, bid) => {
+  cardTypes[type].set(hand, bid);
+};
+
+const setRanks = (typeMap) => {
+  typeMap.forEach((value, key) => {});
+};
+
+console.log(`Part One: `, partOne());
