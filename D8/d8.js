7d32309da5d1a5f977ca03e20ec5ff099ca08099
Monica Hong <momo.star@gmail.com>
2023-12-10 01:30:31 -0800

    struggle d8 p2

--- D8/d8.js
+++ D8/d8.js
@@ -1,12 +1,13 @@
 var fs = require("fs");
 var file = fs.readFileSync("./D8/d8.txt", "utf-8");
 // var file = fs.readFileSync("./D8/test.txt", "utf-8");
+// var file = fs.readFileSync("./D8/test2.txt", "utf-8");
 var rows = file.split("\r\n");
 
 const partOne = (inputs) => {
   let networkMap = createMap(inputs);
 
-  let steps = readSteps(inputs);
+  let steps = getStepsArray(inputs);
 
   let count = 0;
   let move = "AAA";
@@ -21,7 +22,33 @@ const partOne = (inputs) => {
 };
 
 const partTwo = (inputs) => {
-  return 0;
+  let networkMap = createMap(inputs);
+  let steps = getStepsArray(inputs);
+  let stepsLen = steps.length;
+  // simultaneously start from nodes that end with A
+  let startNodes = getStartNodes(inputs);
+  let startNodesLen = startNodes.length;
+  let endNodes = getEndNodes(inputs);
+  let endNodesSet = new Set(endNodes);
+  let count = 0;
+  let index = 0;
+
+  while (!startNodes.every((node) => endNodesSet.has(node))) {
+    let step = steps[index];
+    // track all progressions
+    for (let n = 0; n < startNodesLen; n++) {
+      let node = startNodes[n];
+      startNodes[n] = moveCycle(networkMap, node, step);
+    }
+    count++;
+    if (index < stepsLen - 1) {
+      index++;
+    } else {
+      index = 0;
+    }
+  }
+  // total steps till all end on nodes ending with Z
+  return count;
 };
 
 const createMap = (inputs) => {
@@ -36,7 +63,7 @@ const createMap = (inputs) => {
   return inputMap;
 };
 
-const readSteps = (inputs) => {
+const getStepsArray = (inputs) => {
   let steps;
   for (let i = 0; i < inputs.length; i++) {
     let input = inputs[i];
@@ -59,6 +86,32 @@ const moveCycle = (map, key, dir) => {
   if (dir === "R") {
     return thisMap.right;
   }
+};
+
+const getStartNodes = (inputs) => {
+  let startNodes = [];
+  for (let i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+    if (!input.match(/^\w{2}A/)) {
+      continue;
+    }
+    let start = input.match(/^\w{2}A/);
+    startNodes.push(start[0]);
+  }
+  return startNodes;
+};
+
+const getEndNodes = (inputs) => {
+  let endNodes = [];
+  for (let i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+    if (!input.match(/^\w{2}Z/)) {
+      continue;
+    }
+    let start = input.match(/^\w{2}Z/);
+    endNodes.push(start[0]);
+  }
+  return endNodes;
 };
 
 // console.log(`Part One: `, partOne(rows));
